42ebd56be624d3091c973696c97406f26097a4e2
DhanukaRathnayake <64305009+DhanukaRathnayake@users.noreply.github.com>
2024-07-03 07:33:33 +0530

    test - nextjs

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // output: 'export',
+  basePath: "/my-portfolio-site",
+  assetPrefix: "/my-portfolio-site/",
+};
+
+export default nextConfig;
