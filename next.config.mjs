e0ae1d99c9c57813912291ae4e4cc773f6518dc5
DhanukaRathnayake <64305009+DhanukaRathnayake@users.noreply.github.com>
2024-09-24 13:03:43 +0530

    Update to private repo and make forebase hosting

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // output: "export",
-  basePath: "/my-portfolio-site",
-  assetPrefix: "/my-portfolio-site/",
+  output: "export",
+  // basePath: "/my-portfolio-site",
+  // assetPrefix: "/my-portfolio-site/",
   images: {
     unoptimized: true,
   },
