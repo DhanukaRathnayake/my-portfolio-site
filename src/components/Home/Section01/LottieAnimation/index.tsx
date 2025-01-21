410bcb82104cad090c81f03884fe8f9d6c0e7364
DhanukaRathnayake <64305009+DhanukaRathnayake@users.noreply.github.com>
2025-01-21 09:55:17 +0530

    update

--- src/components/Home/Section01/LottieAnimation/index.tsx
+++
@@ -1,7 +0,0 @@
-// components/LottieAnimation.tsx
-import Lottie from "lottie-react";
-import animationData from "../../../../../public/LottieAnimaton.json"; // Import your Lottie JSON file
-
-export default function LottieAnimation() {
-  return <Lottie animationData={animationData} loop={true} />;
-}
