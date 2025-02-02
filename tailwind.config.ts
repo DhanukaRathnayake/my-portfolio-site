1fd22dfd65d2a0a1a0454fdce5818b46b809c636
DhanukaRathnayake <64305009+DhanukaRathnayake@users.noreply.github.com>
2025-02-02 10:02:48 +0530

    Update npm successfully

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,6 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  compilerOptions: {
-    types: ["react-icons"],
-  },
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
